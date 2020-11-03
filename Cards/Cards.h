47e04d1c1781cab76f07d4b4b32a93585d283a30
EbrahLibra <60298419+EbrahLibra@users.noreply.github.com>
2020-11-03 08:07:27 -0500

    Add files via upload

--- Cards/Cards.h
+++ Cards/Cards.h
@@ -1,78 +1,123 @@
-//
-//  Cards.h
-//  assignment1
-//
-//  Created by Ebraheem Al Shapi.
-//
-
-
-
-
+// Author Ebraheem Al Shapi
+// Solution Risk
+// Project: Cards
+// Created at 1:34 AM 19/10/2020
+// Date: 02/11/2020   8:59 AM
+// File Name: Cards.h
 
 #pragma once
+
+//#define _DEBUG
+//#ifdef _DEBUG
+//#define new new( _NORMAL_BLOCK , __FILE__ , __LINE__ )
+//#endif
+
+
 #include <array>
-#include <list> 
-#include <string>
 #include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
-enum CardType { spy, 
-                bomb, 
-                reinforcement, 
-                blockade, 
-                airlift, 
-                diplomacy};
+enum class CardType
+{
+	spy,
+	bomb,
+	reinforcement,
+	blockade,
+	airlift,
+	diplomacy
+};
 
-ostream& operator<<(std::ostream& out, const CardType value);
+ostream& operator<<(std::ostream& out, CardType value);
 
+//Forward Declaration
 class Hand;
 class Deck;
 
-class Card {
+class Card
+{
+	//Private Members
 private:
-    static unsigned int nextCardID;
-    unsigned int cardID;
-    CardType cardType;
+	static unsigned int nextCardID;
+	unsigned int cardID;
+	CardType* cardType;
 
+	//Private Methods
+private:
+	friend ostream& operator<<(ostream& strm, const Card& card);
+
+	//Public Methods
 public:
-    Card();
-    ~Card();
-    //Card(const Card &card);
-    Card(CardType armyType);
-    CardType getArmyType() const;
-    void setArmyType(CardType &armytype);
-    int getCardID() const;
-    void play(Hand& hand,Deck& deck);
-    friend ostream& operator<<(ostream& strm, const Card& card);
+	//Constructors
+	Card();
+	Card(const Card& card);
+	Card(CardType& armyType);
+
+	//Other Functions
+	CardType& getCardType() const;
+	void setCardType(CardType& armytype);
+	int getCardID() const;
+	void play(Hand& hand, Deck& deck);
+
+	//Destructors
+	~Card();
 };
 
-class Deck{
+class Deck
+{
+	//Private Members
 private:
-    list<Card> allCards;
-    list<Card*> deckCards;
-    void generateCard(int numberOfCounteries);
+	vector<Card*>* deckCards;
+	unsigned int* cardID;
+	void generateCard(int numberOfCounteries);
+
+	//Public Members
 public:
-    Deck();
-    ~Deck();
-    Deck(int numberOfCounteries);
-    Card* draw();
-    void draw(Hand& hands);
-    friend ostream& operator<<(ostream& strm, const Deck& deck);
-    list<Card> getAllCards() const;
-    void setAllCards(list<Card> allCards);
-    list<Card*>& getDeckCards();
-    void setDeckCards(list<Card*> deckCards);
+
+	//Private Methods
+private:
+	friend ostream& operator<<(ostream& strm, const Deck& deck);
+
+	//Public Methods
+public:
+	//Constructors
+	Deck();
+	Deck(int numberOfCounteries);
+	Deck(Deck& numberOfCounteries);
+
+	//Other Functions
+	Card* draw();
+	void draw(Hand& hands);
+	vector<Card*>* getDeckCards();
+	void setDeckCards(vector<Card*>& deckCards);
+
+	//Destructors
+	~Deck();
 };
 
 
-class Hand {
+class Hand
+{
+	//Private Members
 private:
-    list<Card*> handCards;
-public:
-    Hand();
-    ~Hand();
-    friend ostream& operator<<(ostream& strm, const Hand hand);
-    list<Card*>& getHandCards();
-    void setHandCards(list<Card*> handCards);
+	vector<Card*> handCards;
 
+	//Public Members
+public:
+
+	//Private Methods
+private:
+	friend ostream& operator<<(ostream& strm, Hand hand);
+public:
+	//Constructors
+	Hand();
+
+	//Other Functions
+	vector<Card*>& getHandCards();
+	vector<Card*> getHandCards() const;
+	void setHandCards(vector<Card*>& handCards);
+
+	//Destructors
+	~Hand();
 };
